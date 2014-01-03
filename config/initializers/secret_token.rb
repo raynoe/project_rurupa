f60923ae378e1e8718b16d74fb246f68d562b308
Raynoe <raynoe_lxd@hotmail.com>
2014-01-03 16:01:59 +0700

    create a first app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ProjectApi::Application.config.secret_key_base = '4b0b4d51f6b7e2c3f964b96e78c4cbb169205ddb1493ba410164cee43a75f3a9ec8a65ea4e407311b9b1e39e63f834b42ce0a7b150c0dbdb9492687736a5219e'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+ProjectApi::Application.config.session_store :cookie_store, key: '_project_api_session'
